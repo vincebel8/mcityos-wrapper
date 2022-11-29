8162a8469545d1a1887ea2037161b4111510882a
Vince Belanger <vincebel@umich.edu>
2022-11-29 15:56:18 -0500

    fix example.py

--- example.py
+++ example.py
@@ -4,4 +4,4 @@ from mcityoswrapper import McityOSIntersections
 #print(McityOSRail.get_railcrossings())
 print(McityOSIntersections.get_intersection(1))
 
-print(McityOSIntersections.trigger_rail(1))
+print(McityOSRail.trigger_railcrossing(1))
